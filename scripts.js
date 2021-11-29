1652509ab302441132ede040800679852f776b11
Komal Gill <komalgill0310@gmail.com>
2021-11-28 23:07:12 -0500

    able to display the clicked image & created new html file to display clicked image

--- scripts.js
+++ scripts.js
@@ -1,17 +1,71 @@
-//pseudocode for: able to select any of the three images
-//need to use mouse event values
-//whole RPS is based on mouse's left keydown button
-//first try to link all of the images to the mouse event
-//when user click on any of the three images then the screen should be able to display which mouse button was pressed
+let img = document.createElement('img');
 
-function getRandomImage(){
-  const images = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjbCbt-tpSLG0mW8ftMQqX1TLPh9zUh7-MoQ&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6F0pt5tNfkfpz8DYZ06UH4eUJV6GeOfg8kg&usqp=CAU","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLhxb4fESwYvjnFNbcai9MT6jBrE68GensGg&usqp=CAU"];
-  let randomImageIndex = Math.floor(Math.random()*images.length);
-  console.log(randomImageIndex);
-  const createImageElement = document.createElement('img');
-  console.log(createImageElement);
-  createImageElement.src = images[randomImageIndex];
-  document.body.appendChild(createImageElement);
-}
+let image = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjbCbt-tpSLG0mW8ftMQqX1TLPh9zUh7-MoQ&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6F0pt5tNfkfpz8DYZ06UH4eUJV6GeOfg8kg&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLhxb4fESwYvjnFNbcai9MT6jBrE68GensGg&usqp=CAU"];
+
+document.getElementById('rock').addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('Rock');
+  img.src = image[0];
+  document.body.appendChild(img);
+});
+
+document.getElementById('paper').addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('Paper');
+  img.src = image[1];
+  document.body.appendChild(img);
+});
+
+document.getElementById('scissor').addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('Scissor');
+  img.src = image[2];
+  document.body.appendChild(img);
+});
 
 
+
+
+
+
+// images.forEach(image => {
+  //   image.addEventListener('click', (e) => {
+  //     e.preventDefault();
+  //     if (image[0].clicked) {
+  //       console.log('Rock');
+  //       img.src = image[0];
+  //       // document.body.appendChild(img);
+  //     }
+  //     else if (image[1]) {
+  //       console.log('Paper');
+  //       img.src = image[1];
+  //       // document.body.appendChild(img);
+  //     }
+  //     else {
+  //       console.log('Scissor');
+  //       img.src = image[2];
+  //       // document.body.appendChild(img);
+  //     }
+  //     return document.body.appendChild(img);
+  // });
+  // });
+
+  // images.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   if (image[0]) {
+//     console.log('Rock');
+//     img.src = image[0];
+//     // document.body.appendChild(img);
+//   }
+//   else if (image[1]) {
+//     console.log('Paper');
+//     img.src = image[1];
+//     // document.body.appendChild(img);
+//   }
+//   else {
+//     console.log('Scissor');
+//     img.src = image[2];
+//     // document.body.appendChild(img);
+//   }
+//   return document.body.appendChild(img);
+// });
