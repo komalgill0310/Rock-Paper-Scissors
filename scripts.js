ea2c15160b0f58a45f069ce27d9572dfbff84263
Komal Gill <komalgill0310@gmail.com>
2021-11-29 22:25:12 -0500

    radomImage is displaying on the page

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let img = document.createElement('img');
+let randImg = document.createElement('img');
 
 let image = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjbCbt-tpSLG0mW8ftMQqX1TLPh9zUh7-MoQ&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6F0pt5tNfkfpz8DYZ06UH4eUJV6GeOfg8kg&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLhxb4fESwYvjnFNbcai9MT6jBrE68GensGg&usqp=CAU"];
 
@@ -7,6 +8,7 @@ document.getElementById('rock').addEventListener('click', (e) => {
   console.log('Rock');
   img.src = image[0];
   document.body.appendChild(img);
+  setTimeout(getRandomImage(),1000);
 });
 
 document.getElementById('paper').addEventListener('click', (e) => {
@@ -14,6 +16,7 @@ document.getElementById('paper').addEventListener('click', (e) => {
   console.log('Paper');
   img.src = image[1];
   document.body.appendChild(img);
+  setTimeout(getRandomImage(),1000);
 });
 
 document.getElementById('scissor').addEventListener('click', (e) => {
@@ -21,7 +24,17 @@ document.getElementById('scissor').addEventListener('click', (e) => {
   console.log('Scissor');
   img.src = image[2];
   document.body.appendChild(img);
+  setTimeout(getRandomImage(),1000);
 });
+
+function getRandomImage() {
+  let randomImageIndex = Math.floor(Math.random() * image.length);
+  randImg.src = image[randomImageIndex];
+  document.body.appendChild(randImg);
+  console.log("random Function worked");
+}
+
+
 
 
 
